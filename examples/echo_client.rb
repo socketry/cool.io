50f394cf0edf5c7ba54d29fecfd7251774023479
rogerdpack <rogerdpack@ea6e6bc6-682b-4e20-bb59-304e6edd756d>
2008-07-25 08:37:37 +0000

    add client example
    git-svn-id: http://rev.rubyforge.org/svn/trunk@224 ea6e6bc6-682b-4e20-bb59-304e6edd756d

---
+++ examples/echo_client.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + '/../lib/rev'
+
+ADDR = '127.0.0.1'
+PORT = 4321
+
+class ClientConnection < Rev::TCPSocket
+  def on_connect
+    puts "#{remote_addr}:#{remote_port} connected"
+    write "bounce this back to me"
+  end
+
+  def on_close
+    puts "#{remote_addr}:#{remote_port} disconnected"
+  end
+
+  def on_read(data)
+    print "got #{data}"
+    close
+  end
+
+end
+
+event_loop = Rev::Loop.default
+a = ClientConnection.connect(ADDR, PORT).attach(event_loop)
+puts "Echo client started to #{ADDR}:#{PORT}"
+event_loop.run
