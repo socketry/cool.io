0f386b39d11ad40e0377640b884a08d4def2fe7b
rogerdpack <rogerdpack@ea6e6bc6-682b-4e20-bb59-304e6edd756d>
2008-08-01 04:50:45 +0000

    better comments
    git-svn-id: http://rev.rubyforge.org/svn/trunk@240 ea6e6bc6-682b-4e20-bb59-304e6edd756d

--- examples/echo_client.rb
+++ examples/echo_client.rb
@@ -18,10 +18,18 @@ class ClientConnection < Rev::TCPSocket
     close
   end
 
+  def on_resolve_failed
+    print "DNS resolve failed"
+  end
+
+  def on_connect_failed
+    print "connect failed, meaning our connection to their port was rejected"
+  end
+
 end
 
 event_loop = Rev::Loop.default
 client = ClientConnection.connect(ADDR, PORT)
 client.attach(event_loop)
-puts "Echo client started to #{ADDR}:#{PORT}"
-event_loop.run
+puts "Echo client connecting to #{ADDR}:#{PORT}..."
+event_loop.run 
