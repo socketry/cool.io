ccb25d3f2a97c234ebf613757f2ee59179dda249
Eric Wong <normalperson@yhbt.net>
2009-12-02 23:58:23 -0800

    timer_watcher_spec: ensure interval has passed before running
    Otherwise Rev::Loop.run_once may not have fired

--- spec/timer_watcher_spec.rb
+++ spec/timer_watcher_spec.rb
@@ -2,12 +2,15 @@ require File.dirname(__FILE__) + '/../lib/rev'
 
 describe Rev::TimerWatcher do
 
+  interval = 0.010
+
   it "can have the on_timer callback defined after creation" do
-    @watcher = Rev::TimerWatcher.new(0.01, true)
+    @watcher = Rev::TimerWatcher.new(interval, true)
     nr = '0'
     @watcher.on_timer { nr.succ! }.should == nil
     @watcher.attach(Rev::Loop.default).should == @watcher
     nr.should == '0'
+    sleep interval
     Rev::Loop.default.run_once
     nr.should == '1'
   end
@@ -20,25 +23,28 @@ describe Rev::TimerWatcher do
         TMP.succ!
       end
     end
-    @watcher = MyTimerWatcher.new(0.01, true)
+    @watcher = MyTimerWatcher.new(interval, true)
     @watcher.attach(Rev::Loop.default).should == @watcher
     MyTimerWatcher::TMP.should == '0'
+    sleep interval
     Rev::Loop.default.run_once
     MyTimerWatcher::TMP.should == '1'
   end
 
   it "can have the on_timer callback redefined between runs" do
-    @watcher = Rev::TimerWatcher.new(0.01, true)
+    @watcher = Rev::TimerWatcher.new(interval, true)
     nr = '0'
     @watcher.on_timer { nr.succ! }.should == nil
     @watcher.attach(Rev::Loop.default).should == @watcher
     nr.should == '0'
+    sleep interval
     Rev::Loop.default.run_once
     nr.should == '1'
     @watcher.detach
     @watcher.on_timer { nr = :foo }.should == nil
     @watcher.attach(Rev::Loop.default).should == @watcher
     nr.should == '1'
+    sleep interval
     Rev::Loop.default.run_once
     nr.should == :foo
   end
