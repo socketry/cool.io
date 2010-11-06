3a0414886c7f8aeb7fd6b41e34b00d60d62d205c
Tony Arcieri <tony@medioh.com>
2010-11-06 16:21:14 -0600

    Give the EventMachine emulation support a little bit of love

--- lib/rev/eventmachine.rb
+++ lib/rev/eventmachine.rb
@@ -1,28 +1,21 @@
 #--
-# Copyright (C)2007 Tony Arcieri
+# Copyright (C)2007-10 Tony Arcieri, Roger Pack
 # You can redistribute this under the terms of the Ruby license
 # See file LICENSE for details
 #++
 
-
-# This file creates a an EventMachine class
-# that is actually using rev as its underlying socket layer, but uses the EM API.
-# require it instead of eventmachine
-# i.e. 
-# require 'revem'
-# instead of
-# require 'eventmachine'
-# Note: you may want to do both a require 'eventmachine' THEN a require 'revem'
-# so that eventmachine is loaded once, then rev overrides it, then there's no require confusion 
-# (if simple servers hang, requiring 'eventmachine' after 'revem' could be the cause of it).
-# drawbacks: slightly slower than EM.
-# benefits: timers are more accurate using libev than using EM.  Also rev is sometimes more compatible than EM (ex: 1.9 windows)
+# EventMachine emulation for Cool.io:
+#
+#   require 'coolio/eventmachine'
+#
+# Drawbacks: slightly slower than EM.
+# Benefits: timers are more accurate using libev than using EM
 # TODO: some things like connection timeouts aren't implemented yet
 # DONE: timers and normal socket functions are implemented.
-require File.dirname(__FILE__) + '/rev'
+
+require 'rev'
 
 module EventMachine
-  
   class << self
     # Start the Reactor loop
     def run
@@ -48,30 +41,34 @@ module EventMachine
     # ltodo: use Rev's PeriodicTimer to wrap EM's two similar to it
     # todo: close all connections on 'stop', I believe
 
-    def add_timer interval, proc = nil, &block
+    def add_timer(interval, proc = nil, &block)
       block ||= proc
       t = OneShotEMTimer.new(interval, false) # non repeating
       t.setup(block) 
-      t.attach(Rev::Loop.default) # fire 'er off ltodo: do we keep track of these timers in memory?
+      
+      # fire 'er off ltodo: do we keep track of these timers in memory?
+      t.attach(Rev::Loop.default)
       t
     end
 
-    def cancel_timer t
-     t.detach if t.attached? # guess there's a case where EM you can say 'cancel' but it's already fired? kind of odd but it happens
+    def cancel_timer(t)
+      # guess there's a case where EM you can say 'cancel' but it's already fired?
+      # kind of odd but it happens
+     t.detach if t.attached? 
     end
 
-    def set_comm_inactivity_timeout *args; end # TODO
+    def set_comm_inactivity_timeout(*args); end # TODO
 
     # Make an outgoing connection
     def connect(addr, port, handler = Connection, *args, &block)
-	block = args.pop if Proc === args[-1]
+      block = args.pop if Proc === args[-1]
 
-       # make sure we're a 'real' class here
-       klass = if (handler and handler.is_a?(Class))
-         handler
-       else
-         Class.new( Connection ) {handler and include handler}
-       end
+      # make sure we're a 'real' class here
+      klass = if (handler and handler.is_a?(Class))
+        handler
+      else
+        Class.new( Connection ) {handler and include handler}
+      end
 
       wrapped_child = CallsBackToEM.connect(addr, port, *args) # ltodo: args? what? they're used? also TODOC TODO FIX
       conn = klass.new(wrapped_child) # ltodo [?] addr, port, *args)
@@ -83,25 +80,25 @@ module EventMachine
     
     # Start a TCP server on the given address and port
     def start_server(addr, port, handler = Connection, *args, &block)
-
-       # make sure we're a 'real' class here
-       klass = if (handler and handler.is_a?(Class))
-         handler
-       else
-         Class.new( Connection ) {handler and include handler}
-       end
-      server = Rev::TCPServer.new(addr, port, CallsBackToEM, *args) { |wrapped_child| 
-	conn = klass.new(wrapped_child)
-	conn.heres_your_socket(wrapped_child) # ideally NOT have this :)
-	wrapped_child.call_back_to_this(conn) 
-	block.call(conn) if block
-      }
+      # make sure we're a 'real' class here
+      klass = if (handler and handler.is_a?(Class))
+        handler
+      else
+        Class.new( Connection ) {handler and include handler}
+      end
+      
+      server = Rev::TCPServer.new(addr, port, CallsBackToEM, *args) do |wrapped_child| 
+        conn = klass.new(wrapped_child)
+        conn.heres_your_socket(wrapped_child) # ideally NOT have this :)
+        wrapped_child.call_back_to_this(conn) 
+        block.call(conn) if block
+      end
 
       server.attach(Rev::Loop.default)
     end
 
-    def stop_server server
-	server.close
+    def stop_server(server)
+      server.close
     end
 
     # Set the maximum number of descriptors available to this process
@@ -122,7 +119,6 @@ module EventMachine
       def on_timer
        @parent.connection_has_timed_out
       end
-    
     end
 
     def call_back_to_this parent
@@ -131,17 +127,26 @@ module EventMachine
     end
 
     def on_connect
-      # @connection_timer.detach if @connection_timer # won't need that anymore :) -- with server connecteds we don't have it, anyway
-      @call_back_to_this.connection_completed if @call_back_to_this # TODO should server accepted's call this? They don't currently [and can't, since on_connect gets called basically in the initializer--needs some code love for that to happen :)
+      # @connection_timer.detach if @connection_timer 
+      # won't need that anymore :) -- with server connecteds we don't have it, anyway
+      
+      # TODO should server accepted's call this? They don't currently
+      # [and can't, since on_connect gets called basically in the initializer--needs some code love for that to happen :)
+      @call_back_to_this.connection_completed if @call_back_to_this
     end
 
     def connection_has_timed_out
       return if closed?
-      close unless closed? # wonder if this works when you're within a half-connected phase.  I think it does.  What about TCP state?
+      
+      # wonder if this works when you're within a half-connected phase.
+      # I think it does.  What about TCP state?
+      close unless closed? 
       @call_back_to_this.unbind
     end
 
-    def on_write_complete; close if @should_close_after_writing; end
+    def on_write_complete
+      close if @should_close_after_writing
+    end
 
     def should_close_after_writing
       @should_close_after_writing = true;
@@ -168,7 +173,7 @@ module EventMachine
       @call_back_to_this.unbind
     end
 
-    def self.connect *args
+    def self.connect(*args)
       a = super *args
       # the connect timer currently kills TCPServer classes.  I'm not sure why.
       #@connection_timer = ConnectTimer.new(14) # needs to be at least higher than 12 :)
@@ -176,26 +181,29 @@ module EventMachine
       #@connection_timer.attach(Rev::Loop.default)
       a
     end
-  
   end
 
   class Connection
-    def self.new *args
+    def self.new(*args)
       allocate#.instance_eval do
       #  initialize *args
       #end
     end
+    
     # we will need to call 'their functions' appropriately -- the commented out ones, here
     # 
     # Callback fired when connection is created
-    def post_init; end # I thought we were 'overriding' EM's existing methods, here.  Huh? Why do we have to define these then?
+    def post_init
+      # I thought we were 'overriding' EM's existing methods, here.  
+      # Huh? Why do we have to define these then?
+    end
 
     # Callback fired when connection is closed
     def unbind; end
     
     # Callback fired when data is received
     # def receive_data(data); end
-    def heres_your_socket instantiated_rev_socket
+    def heres_your_socket(instantiated_rev_socket)
       instantiated_rev_socket.call_back_to_this self
       @wrapped_rev = instantiated_rev_socket
     end
@@ -217,9 +225,11 @@ module EventMachine
     end
 
     def get_peername
-        family, port, host_name, host_ip = @wrapped_rev.peeraddr
-	Socket.pack_sockaddr_in( port, host_ip) # pack it up :)
+      family, port, host_name, host_ip = @wrapped_rev.peeraddr
+      Socket.pack_sockaddr_in(port, host_ip) # pack it up :)
     end
-    
   end
 end
+
+# Shortcut constant
+EM = EventMachine
