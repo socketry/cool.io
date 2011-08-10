82d688e536f74720d9cd4c5bf966a42290c27357
Tony Arcieri <tony.arcieri@gmail.com>
2011-08-10 00:16:27 -0700

    Better versions of these tests coming soon in Cool.io 2.0

--- spec/possible_tests/test_on_resolve_failed.rb
+++
@@ -1,9 +0,0 @@
-require "rev"
-class Tester < Rev::TCPSocket
-  def on_resolve_failed
-    print "resolved failed! that's good!"
-  end
-end
-a = Tester.connect('asdfgoogle.com', 80)
-a.attach Rev::Loop.default
-Rev::Loop.default.run
