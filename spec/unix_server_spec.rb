fccfe437a586c406fb8e1edef097fc2936336ab7
Masahiro Nakagawa <repeatedly@gmail.com>
2013-07-27 04:19:43 +0900

    Add Listener#listen method to change backlog size

--- spec/unix_server_spec.rb
+++ spec/unix_server_spec.rb
@@ -11,13 +11,15 @@ describe Cool.io::UNIXServer, :env => :win do
 
   it "creates a new Cool.io::UNIXServer" do
     listener = Cool.io::UNIXListener.new(@tmp.path)
+    listener.listen(24)
     File.socket?(@tmp.path).should == true
   end
 
   it "builds off an existing ::UNIXServer" do
     unix_server = ::UNIXServer.new(@tmp.path)
     File.socket?(@tmp.path).should == true
-    listener = Cool.io::UNIXServer.new(unix_server)
+    listener = Cool.io::UNIXServer.new(unix_server, Coolio::UNIXSocket)
+    listener.listen(24)
     File.socket?(@tmp.path).should == true
     listener.fileno.should == unix_server.fileno
   end
