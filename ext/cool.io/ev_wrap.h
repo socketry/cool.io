44562efeedb317fecdcbe60b802d86ec361b6f61
Masahiro Nakagawa <repeatedly@gmail.com>
2014-04-05 00:37:49 +0900

    Remove unnecessary definition and disable EV_USE_MONOTONIC and EV_USE_REALTIME on windows

--- ext/cool.io/ev_wrap.h
+++ ext/cool.io/ev_wrap.h
@@ -1,7 +1,9 @@
 #define EV_STANDALONE              /* keeps ev from requiring config.h */
+
 #ifdef _WIN32
-# define EV_SELECT_IS_WINSOCKET 1   /* configure libev for windows select */
-# define FD_SETSIZE 2048 /* wishful thinking, as msvcrt6 [?] seems to only allow 512 fd's and 256 sockets max */
+#define EV_SELECT_IS_WINSOCKET 1   /* configure libev for windows select */
+#define EV_USE_MONOTONIC 0
+#define EV_USE_REALTIME 0
 #endif
 
 #include "../libev/ev.h"
