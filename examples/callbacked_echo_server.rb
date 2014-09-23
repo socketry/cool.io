c1c9a7ddfc50a6f1fedfad8f600bad79a2ff71c1
Kouhei Sutou <kou@clear-code.com>
2014-09-23 23:31:05 +0900

    Don't change callback evaluation scope
    CAUTION: THIS IS BACKWARD INCOMPATIBLE CHANGE!
    
    Normally, block is evaluated without changing `self` in Ruby. For
    example:
    
        elements = [1]
        elements.each do |element|
          p [self, elements] # => [main, [1]]
        end
    
    But callback block in Cool.io is evaluated with changing `self` to
    object that has the event. For example:
    
        require "cool.io"
    
        event_loop = Cool.io::Loop.default
        server = Cool.io::TCPServer.new("127.0.0.1", 2929) do |connection|
          connection.on_close do
            p(self == connection) # => true
          end
        end
        server.attach(event_loop)
        event_loop.run
    
    We need to create a wrapper Proc to evaluate callback block in original
    scope such as:
    
        require "cool.io"
    
        event_loop = Cool.io::Loop.default
        server = Cool.io::TCPServer.new("127.0.0.1", 2929) do |connection|
          self_in_tcp_server_new_block = self
          on_close = lambda do
            p(self == connection) # => false
            p(self == self_in_tcp_server_new_block) # => true
          end
          connection.on_close do
            on_close.call
          end
        end
        server.attach(event_loop)
        event_loop.run
    
    The current API is inconsistent with built-in Ruby methods. API with
    this change is consistent with built-in Ruby methods.

--- examples/callbacked_echo_server.rb
+++ examples/callbacked_echo_server.rb
@@ -11,11 +11,11 @@ server = Cool.io::TCPServer.new(ADDR, PORT) do |connection|
   puts "#{connection.remote_addr}:#{connection.remote_port} connected"
 
   connection.on_close do
-    puts "#{remote_addr}:#{remote_port} disconnected"
+    puts "#{connection.remote_addr}:#{connection.remote_port} disconnected"
   end
 
   connection.on_read do |data|
-    write data
+    connection.write data
   end
 end
 server.attach(event_loop)
