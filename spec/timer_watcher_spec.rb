498c314e298299e10a7e1ecd3162515d040adb86
SATO taichi <ryushi@gmail.com>
2015-02-04 23:16:21 +0900

    use new instance instead of default instance.

--- spec/timer_watcher_spec.rb
+++ spec/timer_watcher_spec.rb
@@ -4,14 +4,18 @@ describe Cool.io::TimerWatcher do
 
   interval = 0.010
 
+  let :loop do
+    Cool.io::Loop.new
+  end
+
   it "can have the on_timer callback defined after creation" do
     @watcher = Cool.io::TimerWatcher.new(interval, true)
     nr = '0'
     @watcher.on_timer { nr.succ! }.should == nil
-    @watcher.attach(Cool.io::Loop.default).should == @watcher
+    @watcher.attach(loop).should == @watcher
     nr.should == '0'
     sleep interval
-    Cool.io::Loop.default.run_once
+    loop.run_once
     nr.should == '1'
   end
 
@@ -24,10 +28,10 @@ describe Cool.io::TimerWatcher do
       end
     end
     @watcher = MyTimerWatcher.new(interval, true)
-    @watcher.attach(Cool.io::Loop.default).should == @watcher
+    @watcher.attach(loop).should == @watcher
     MyTimerWatcher::TMP.should == '0'
     sleep interval
-    Cool.io::Loop.default.run_once
+    loop.run_once
     MyTimerWatcher::TMP.should == '1'
   end
 
@@ -35,17 +39,17 @@ describe Cool.io::TimerWatcher do
     @watcher = Cool.io::TimerWatcher.new(interval, true)
     nr = '0'
     @watcher.on_timer { nr.succ! }.should == nil
-    @watcher.attach(Cool.io::Loop.default).should == @watcher
+    @watcher.attach(loop).should == @watcher
     nr.should == '0'
     sleep interval
-    Cool.io::Loop.default.run_once
+    loop.run_once
     nr.should == '1'
     @watcher.detach
     @watcher.on_timer { nr = :foo }.should == nil
-    @watcher.attach(Cool.io::Loop.default).should == @watcher
+    @watcher.attach(loop).should == @watcher
     nr.should == '1'
     sleep interval
-    Cool.io::Loop.default.run_once
+    loop.run_once
     nr.should == :foo
   end
 
