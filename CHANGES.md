02d48a1d1dad7d2c432732588e20788402a60eb0
Hiroshi Hatake <hatake@calyptia.com>
2021-04-27 14:45:10 +0900

    v1.7.2
    Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,8 @@
+1.7.2
+-----
+
+* Add ruby 3.0 to windows gem
+
 1.7.1
 -----
 
--- lib/cool.io/version.rb
+++ lib/cool.io/version.rb
@@ -1,6 +1,6 @@
 module Coolio
-  VERSION = "1.7.1"
-  
+  VERSION = "1.7.2"
+
   def self.version
     VERSION
   end
